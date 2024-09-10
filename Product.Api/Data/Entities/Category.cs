04cba8933bb6b0723e973d8b052ff5610a6f64bd
Ian <ianmaciel76@gmail.com>
2024-09-10 08:24:39 -0300

    adicionar "Entities"

---
+++ Product.Api/Data/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Product.Api.Data.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required ICollection<Product> Products { get; init; }
+    }
+}
---
+++ Product.Api/Data/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Product.Api.Data.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public required string Description { get; set; }
+        public int CategoryId { get; set; }
+        public required Category Category { get; init; }
+
+    }
+}
