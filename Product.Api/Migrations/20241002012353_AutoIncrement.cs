0be55b518608a93beac2eb62336da9db03206afb
Ian <ianmaciel76@gmail.com>
2024-10-02 19:08:52 -0300

    Add auto-increment and relationships to Category and Product
    Introduce migration `20241002012353_AutoIncrement` for `Product.Api`.
    Add auto-increment to `CategoryId` and `ProductId` in `Category`
    and `Product` entities. Update `ParentId` in `Category` and
    `CategoryId` in `Product` to use SQL Server value generation
    strategy. Update `ApplicationDbContextModelSnapshot` to reflect
    these changes, including identity columns and new value generation
    strategy. Add seed data for `Category` and `Product` entities.
    Establish self-referencing relationship in `Category` and
    relationship with cascade delete in `Product`. Update navigation
    properties in `Category` to reflect new relationships and associated
    products.

---
+++ Product.Api/Migrations/20241002012353_AutoIncrement.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Product.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoIncrement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                column: "ParentId",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ParentId" },
+                values: new object[,]
+                {
+                    { 4, "Sub Category 1 Description", "Sub Category 1", 3 },
+                    { 5, "Sub Category 2 Description", "Sub Category 2", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3,
+                column: "ParentId",
+                value: null);
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ParentId" },
+                values: new object[] { 2, "Main Category 2 Description", "Main Category 2", 1 });
+        }
+    }
+}
